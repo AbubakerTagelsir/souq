40049ad2c3f675165649aaea84b122f9fd73ddb6
Abubaker Tagelsir <bakri826.at@gmail.com>
2018-11-15 06:38:06 -0500

    Basic Structure

--- models/models.py
+++ models/models.py
@@ -28,6 +28,7 @@ class SouqOrder(models.Model):
     )
     pickup_location = fields.Char("Pickup Location", help="City - Area - Street")
     total_price = fields.Float("Total Price", compte="_get_the_total_price")
+    num_booking = fields.Integer("Total Bookings", compte="get_the_number_of_bookings")
     
     @api.one
     def _get_name(self):
@@ -40,6 +41,16 @@ class SouqOrder(models.Model):
         for line in self.order_lines:
             total += line.unit_price * line.qty
         self.total_price = total
+
+    def get_the_number_of_bookings(self):
+        t = 0
+        t = len(self.env['souq.booking'].search([('order_id', '=', self.id)]).ids)
+        print(t)
+        self.num_booking = t
+        return t
+    def publish_order(self):
+        self.state = 'available'
+
 
 class SouqOrderLine(models.Model):
     _name = 'souq.order.line'
@@ -54,7 +65,27 @@ class SouqOrderLine(models.Model):
     used_duration = fields.Integer("Used / Months")
 
 
-    
-    
-    
-    
+class OrderBooking(models.Model):
+    _name = 'souq.booking'
+    name = fields.Char("Name", compute="_get_name")
+    order_id = fields.Many2one('souq.order', "Order")
+    delivery_status = fields.Boolean('Delivery Status', related="order_id.delivery")
+    requester_id = fields.Many2one('res.users', "Requester", default=lambda self: self.env.user)
+    state = fields.Selection([('draft', 'Draft'), ('submitted', "Submitted"), ('accepted', "Accepted"), ('rejected', "Rejected")], default='draft')
+    date = fields.Datetime("Date", default=fields.Datetime.now)
+    notes = fields.Text("Notes")
+    requester_location = fields.Char("Requester Location")
+
+    @api.one
+    def _get_name(self):
+        self.name = "ORB-00" + str(self.id)
+
+    def submit_booking(self):
+        self.state = 'submitted'
+
+    def accept_booking(self):
+        self.state = 'accepted'
+
+    def reject_booking(self):
+        self.state = 'rejected'
+        
